2d08c1702531611b3d068a231d7ff0896212fbdf
lisavineberg <38729152+lisavineberg@users.noreply.github.com>
2023-06-14 17:36:12 -0400

    add styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const location = useLocation();
+
+  const isCurrent = (path) => {
+    return (path === location.pathname) ? " nav-item--current" : ""
+  }
+
   return (
     <nav className="nav">
       <ul className="nav-list">
-        <Link to="/">About</Link>
-        <Link to="/education">Education</Link>
-        <Link to="/career">Career</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/volunteer">Volunteer</Link>
-        <Link to="/tech-stack">Tech stack</Link>
-        <Link to="/blog">Blog</Link>
+        <Link className={"nav-item" + isCurrent("/")} to="/">About</Link>
+        <Link className={"nav-item" + isCurrent("/education")} to="/education">Education</Link>
+        <Link className={"nav-item" + isCurrent("/career")} to="/career">Career</Link>
+        <Link className={"nav-item" + isCurrent("/projects")} to="/projects">Projects</Link>
+        <Link className={"nav-item" + isCurrent("/volunteer")} to="/volunteer">Volunteer</Link>
+        <Link className={"nav-item" + isCurrent("/tech-stack")} to="/tech-stack">Tech stack</Link>
+        <Link className={"nav-item" + isCurrent("/blog")} to="/blog">Blog</Link>
       </ul>
     </nav>
   )
